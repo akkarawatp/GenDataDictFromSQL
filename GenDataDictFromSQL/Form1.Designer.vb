6d223dd7aca66db37945efaca61e62859ebebd7f
akkarawatp <akkarawatp@gmail.com>
2017-05-10 17:40:10 +0700

    Initial Commit

---
+++ GenDataDictFromSQL/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.btnBrowseFile = New System.Windows.Forms.Button()
+        Me.txtOutputPath = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtTableName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtDatabaseName = New System.Windows.Forms.TextBox()
+        Me.txtUserID = New System.Windows.Forms.TextBox()
+        Me.txtDataSource = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.lblProgressText = New System.Windows.Forms.Label()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.lblProgressText)
+        Me.GroupBox3.Controls.Add(Me.ProgressBar1)
+        Me.GroupBox3.Controls.Add(Me.btnBrowseFile)
+        Me.GroupBox3.Controls.Add(Me.txtOutputPath)
+        Me.GroupBox3.Controls.Add(Me.Label10)
+        Me.GroupBox3.Controls.Add(Me.btnGenerate)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Controls.Add(Me.txtTableName)
+        Me.GroupBox3.Controls.Add(Me.txtPassword)
+        Me.GroupBox3.Controls.Add(Me.txtDatabaseName)
+        Me.GroupBox3.Controls.Add(Me.txtUserID)
+        Me.GroupBox3.Controls.Add(Me.txtDataSource)
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Controls.Add(Me.Label8)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Location = New System.Drawing.Point(3, 0)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(747, 152)
+        Me.GroupBox3.TabIndex = 3
+        Me.GroupBox3.TabStop = False
+        '
+        'btnBrowseFile
+        '
+        Me.btnBrowseFile.Location = New System.Drawing.Point(712, 79)
+        Me.btnBrowseFile.Name = "btnBrowseFile"
+        Me.btnBrowseFile.Size = New System.Drawing.Size(28, 23)
+        Me.btnBrowseFile.TabIndex = 25
+        Me.btnBrowseFile.Text = "..."
+        Me.btnBrowseFile.UseVisualStyleBackColor = True
+        '
+        'txtOutputPath
+        '
+        Me.txtOutputPath.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtOutputPath.Location = New System.Drawing.Point(126, 79)
+        Me.txtOutputPath.Name = "txtOutputPath"
+        Me.txtOutputPath.Size = New System.Drawing.Size(584, 22)
+        Me.txtOutputPath.TabIndex = 24
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label10.Location = New System.Drawing.Point(6, 81)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(82, 16)
+        Me.Label10.TabIndex = 23
+        Me.Label10.Text = "Output File"
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnGenerate.Location = New System.Drawing.Point(641, 111)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(99, 36)
+        Me.btnGenerate.TabIndex = 22
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label6.Location = New System.Drawing.Point(2, 59)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(94, 16)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "Table Name"
+        '
+        'txtTableName
+        '
+        Me.txtTableName.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtTableName.Location = New System.Drawing.Point(126, 56)
+        Me.txtTableName.Name = "txtTableName"
+        Me.txtTableName.Size = New System.Drawing.Size(204, 22)
+        Me.txtTableName.TabIndex = 16
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(558, 33)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(181, 22)
+        Me.txtPassword.TabIndex = 15
+        '
+        'txtDatabaseName
+        '
+        Me.txtDatabaseName.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtDatabaseName.Location = New System.Drawing.Point(126, 33)
+        Me.txtDatabaseName.Name = "txtDatabaseName"
+        Me.txtDatabaseName.Size = New System.Drawing.Size(204, 22)
+        Me.txtDatabaseName.TabIndex = 14
+        '
+        'txtUserID
+        '
+        Me.txtUserID.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtUserID.Location = New System.Drawing.Point(558, 10)
+        Me.txtUserID.Name = "txtUserID"
+        Me.txtUserID.Size = New System.Drawing.Size(181, 22)
+        Me.txtUserID.TabIndex = 13
+        '
+        'txtDataSource
+        '
+        Me.txtDataSource.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.txtDataSource.Location = New System.Drawing.Point(126, 10)
+        Me.txtDataSource.Name = "txtDataSource"
+        Me.txtDataSource.Size = New System.Drawing.Size(204, 22)
+        Me.txtDataSource.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label7.Location = New System.Drawing.Point(433, 36)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(76, 16)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Password"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label8.Location = New System.Drawing.Point(5, 36)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(121, 16)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Database Name"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label5.Location = New System.Drawing.Point(434, 13)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 16)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "User ID"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label4.Location = New System.Drawing.Point(6, 13)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Data Source"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(9, 124)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(626, 23)
+        Me.ProgressBar1.TabIndex = 26
+        '
+        'lblProgressText
+        '
+        Me.lblProgressText.AutoSize = True
+        Me.lblProgressText.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.lblProgressText.Location = New System.Drawing.Point(12, 108)
+        Me.lblProgressText.Name = "lblProgressText"
+        Me.lblProgressText.Size = New System.Drawing.Size(0, 13)
+        Me.lblProgressText.TabIndex = 27
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(754, 155)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents btnBrowseFile As Button
+    Friend WithEvents txtOutputPath As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtTableName As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtDatabaseName As TextBox
+    Friend WithEvents txtUserID As TextBox
+    Friend WithEvents txtDataSource As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblProgressText As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ GenDataDictFromSQL/Form1.vb
@@ -0,0 +1,385 @@
+﻿Imports System.IO
+Imports System.Data.SqlClient
+Imports LinqDB.ConnectDB
+Imports OfficeOpenXml
+
+Public Class Form1
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        GetSetting()
+    End Sub
+
+    Private Sub CreateSetting()
+        Dim iniFile As String = Application.StartupPath & "\Setting.ini"
+        Dim ini As New IniReader(iniFile)
+        ini.Section = "Setting"
+
+        ini.Write("DataSource", txtDataSource.Text)
+        ini.Write("DbName", txtDatabaseName.Text)
+        ini.Write("DbUserID", txtUserID.Text)
+        ini.Write("DbPassword", txtPassword.Text)
+        ini.Write("TableName", txtTableName.Text)
+        ini.Write("OutputFile", txtOutputPath.Text)
+        ini = Nothing
+
+    End Sub
+
+    Private Sub GetSetting()
+        Dim iniFile As String = Application.StartupPath & "\Setting.ini"
+        If File.Exists(iniFile) = True Then
+            Dim ini As New IniReader(iniFile)
+            ini.Section = "Setting"
+            txtDataSource.Text = ini.ReadString("DataSource")
+            txtDatabaseName.Text = ini.ReadString("DbName")
+            txtUserID.Text = ini.ReadString("DbUserID")
+            txtPassword.Text = ini.ReadString("DbPassword")
+            txtTableName.Text = ini.ReadString("TableName")
+            txtOutputPath.Text = ini.ReadString("OutputFile")
+            ini = Nothing
+        End If
+
+    End Sub
+
+    Private Sub btnBrowseFile_Click(sender As Object, e As EventArgs) Handles btnBrowseFile.Click
+        Dim fle As New SaveFileDialog
+        fle.Filter = "Excel File|*.xlsx"
+        If txtOutputPath.Text.Trim <> "" Then
+            fle.InitialDirectory = New DirectoryInfo(txtOutputPath.Text).FullName
+        End If
+
+        If fle.ShowDialog = DialogResult.OK Then
+            txtOutputPath.Text = fle.FileName
+        End If
+    End Sub
+
+    Private Function ValidateData() As Boolean
+        Dim ret As Boolean = True
+        If txtDataSource.Text.Trim() = "" Then
+            MsgBox("กรุณาระบุ Data source", MsgBoxStyle.OkOnly + 48, "Validate Data")
+            ret = False
+            txtDataSource.Focus()
+        ElseIf txtUserID.Text.Trim = "" Then
+            MsgBox("กรุณาระบุ Database UserID", MsgBoxStyle.OkOnly + 48, "Validate Data")
+            ret = False
+            txtUserID.Focus()
+        ElseIf txtDatabaseName.Text.Trim = "" Then
+            MsgBox("กรุณาระบุ Database Name", MsgBoxStyle.OkOnly + 48, "Validate Data")
+            ret = False
+            txtDatabaseName.Focus()
+        ElseIf txtPassword.Text.Trim = "" Then
+            MsgBox("กรุณาระบุ Database Password", MsgBoxStyle.OkOnly + 48, "Validate Data")
+            ret = False
+            txtPassword.Focus()
+            'ElseIf txtTableName.Text.Trim = "" Then
+            '    MsgBox("กรุณาระบุ Table Name", MsgBoxStyle.OkOnly + 48, "Validate Data")
+            '    ret = False
+            '    txtTableName.Focus()
+        ElseIf txtOutputPath.Text.Trim = "" Then
+            MsgBox("กรุณาระบุ Output Path", MsgBoxStyle.OkOnly + 48, "Validate Data")
+            ret = False
+            txtOutputPath.Focus()
+        End If
+
+        Return ret
+    End Function
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        If ValidateData() = True Then
+            Dim tDt As DataTable = GetAllTable()
+            If txtTableName.Text.Trim <> "" Then
+                tDt.DefaultView.RowFilter = "table_name='" & txtTableName.Text & "'"
+                tDt = tDt.DefaultView.ToTable
+            End If
+
+            If tDt.Rows.Count > 0 Then
+                ProgressBar1.Maximum = tDt.Rows.Count + 3
+                ProgressBar1.Value = 1
+                Application.DoEvents()
+
+                Using ep As New ExcelPackage
+                    Dim i As Integer = 1
+                    Dim ws As ExcelWorksheet = ep.Workbook.Worksheets.Add("Output")
+                    Dim HeaderRow As Integer = 3   'ถ้า Table แรก ให้เริ่มที่ Row ที่ 3
+
+                    For Each tDr As DataRow In tDt.Rows
+                        Dim TableName As String = tDr("table_name")
+                        lblProgressText.Text = "Generate Table " & TableName.ToUpper & " ( " & i & "/" & tDt.Rows.Count & " )"
+                        Application.DoEvents()
+
+                        'หา Column ของ Table ที่ระบุ
+                        Dim cDt As DataTable = GetTableColumn(TableName)
+                        If cDt.Rows.Count > 0 Then
+                            Dim dt As New DataTable
+                            dt.Columns.Add("Column Name")
+                            dt.Columns.Add("Data Type")
+                            dt.Columns.Add("Comment")
+                            dt.Columns.Add("PK")
+                            dt.Columns.Add("UQ")
+                            dt.Columns.Add("Not Null")
+                            dt.Columns.Add("Default")
+
+                            For Each cDr As DataRow In cDt.Rows
+                                Dim dr As DataRow = dt.NewRow
+                                dr("Column Name") = cDr("COLUMN_NAME")
+                                dr("Data Type") = GetFormatColumnTypeName(cDr("TYPE_NAME"), cDr("LENGTH"))
+                                dr("Comment") = GetColumnComment(TableName, cDr("COLUMN_NAME"))
+                                dr("PK") = GetPKColumn(TableName, cDr("COLUMN_NAME"))
+                                dr("UQ") = GetUQColumn(TableName, cDr("COLUMN_NAME"))
+                                dr("Not Null") = IIf(cDr("NULLABLE") = 1, "", "Y")
+                                If Convert.IsDBNull(cDr("COLUMN_DEF")) = False Then dr("Default") = ReplaceBracket(cDr("COLUMN_DEF"))
+
+                                dt.Rows.Add(dr)
+                            Next
+
+                            If dt.Rows.Count > 0 Then
+                                ExportDatatableToExcel(ep, ws, HeaderRow, i & ". " & TableName, dt, GetTableComment(TableName))
+                                HeaderRow += (dt.Rows.Count + 5)
+                            End If
+                            dt.Dispose()
+                        End If
+                        cDt.Dispose()
+                        i += 1
+                        ProgressBar1.Value += 1
+                    Next
+
+                    lblProgressText.Text = "Save output file..."
+                    ProgressBar1.Value += 1
+                    Application.DoEvents()
+
+                    If File.Exists(txtOutputPath.Text) = True Then
+                        File.SetAttributes(txtOutputPath.Text, FileAttributes.Normal)
+                        File.Delete(txtOutputPath.Text)
+                    End If
+
+                    Dim f As New IO.FileInfo(txtOutputPath.Text)
+                    ep.SaveAs(f)
+                    Threading.Thread.Sleep(5000)
+
+                    If IO.File.Exists(f.FullName) = True Then
+                        CreateSetting()
+
+                        lblProgressText.Text = "Generate Complete"
+                        ProgressBar1.Value += 1
+                        Application.DoEvents()
+                        MessageBox.Show("Complete")
+                    End If
+                    f = Nothing
+                End Using
+            End If
+            tDt.Dispose()
+        End If
+    End Sub
+
+    Private Sub ExportDatatableToExcel(ep As ExcelPackage, ws As ExcelWorksheet, HeaderRow As Integer, TableName As String, DT As DataTable, TableComment As String)
+        ws.Cells("A" & (HeaderRow - 2)).Value = TableName.ToUpper
+        ws.Cells("A" & (HeaderRow - 1)).Value = TableComment
+
+        ws.Cells("A" & HeaderRow).LoadFromDataTable(DT, True)
+        Dim hRow As Integer = HeaderRow
+
+        Using RowHeader As ExcelRange = ws.Cells(hRow, 1, hRow, DT.Columns.Count)
+            RowHeader.Style.Font.Bold = True
+            RowHeader.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid
+            RowHeader.Style.Fill.BackgroundColor.SetColor(Color.Gray)
+            RowHeader.Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center
+            RowHeader.Style.Font.Color.SetColor(Color.Black)
+            'RowHeader.AutoFitColumns()
+        End Using
+
+        Using RowContent As ExcelRange = ws.Cells(hRow + 1, 1, hRow + DT.Rows.Count + 1, DT.Columns.Count)
+            RowContent.Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin
+            RowContent.Style.Border.Bottom.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin
+            RowContent.Style.Border.Left.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin
+            RowContent.Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin
+        End Using
+
+        For i As Integer = 0 To DT.Columns.Count - 1
+            Dim ColumType As String = DT.Columns(i).DataType.Name.ToLower
+            If ColumType = "datetime" Then
+                ws.Cells(hRow + 1, i + 1, hRow + DT.Rows.Count + 1, i + 1).Style.Numberformat.Format = "mmm dd yyyy HH:MM:ss"
+            End If
+        Next
+
+        ws.Cells(hRow, 1, hRow + DT.Rows.Count, DT.Columns.Count).AutoFitColumns()
+    End Sub
+    Private Function ReplaceBracket(str As String) As String
+        Dim ret As String = str
+
+        Do
+            If ret.StartsWith("(") = True And ret.EndsWith(")") = True Then
+                ret = ret.Substring(1, ret.Length - 2)
+            End If
+        Loop While ret.StartsWith("(") = True And ret.EndsWith(")") = True
+
+        Return ret
+    End Function
+
+
+#Region "Database Function"
+
+    Private ReadOnly Property GetConnectionString() As String
+        Get
+            Return "Data Source=" & txtDataSource.Text & ";Initial Catalog=" & txtDatabaseName.Text & ";User ID=" & txtUserID.Text & ";Password=" & txtPassword.Text & ";"
+        End Get
+    End Property
+    Private Function GetTableColumn(ByVal tbName As String) As DataTable
+        Dim sql As String = "EXEC SP_COLUMNS " & SqlDB.SetString(tbName)
+        Dim dt As DataTable = SqlDB.ExecuteTable(sql, SqlDB.GetConnection(GetConnectionString))
+        If dt.Rows.Count > 0 Then
+            If dt.Columns.Contains("msrepl_tran_version") = True Then
+                dt.Columns.Remove("msrepl_tran_version")
+            End If
+            If dt.Columns.Contains("rowguid") = True Then
+                dt.Columns.Remove("rowguid")
+            End If
+        End If
+
+        Return dt
+    End Function
+
+    Private Function GetPKColumn(TableName As String, ColumnName As String) As String
+        Dim ret As String = ""
+        Try
+            Dim conn As SqlConnection = SqlDB.GetConnection(GetConnectionString)
+            Dim tmpTable As DataTable = SqlDB.ExecuteTable("EXEC SP_PKEYS " & SqlDB.SetString(TableName), conn)
+            For Each dRow As DataRow In tmpTable.Rows
+                If ColumnName = dRow("column_name").ToString() Then
+                    ret = "Y"
+                    Exit For
+                End If
+            Next
+
+            conn.Close()
+            conn.Dispose()
+
+        Catch ex As Exception
+        End Try
+
+        Return ret
+    End Function
+    Private Function GetUQColumn(TableName As String, ColumnName As String) As String
+        Dim ret As String = ""
+
+        Dim conn As SqlConnection = SqlDB.GetConnection(GetConnectionString)
+        Dim sql As String = "exec sp_indexcolumns_managed " & SqlDB.SetString(txtDatabaseName.Text) & ",null, " & SqlDB.SetString(TableName) & ",null,'" & ColumnName & "'"
+        Dim dt As DataTable = SqlDB.ExecuteTable(sql, conn)
+        If dt.Rows.Count > 0 Then
+            ret = "Y"
+        End If
+        dt.Dispose()
+        conn.Close()
+        conn.Dispose()
+
+        Return ret
+    End Function
+
+    Private Function GetTableComment(TableName As String) As String
+        Dim ret As String = ""
+        Dim sql As String = "SELECT sys.objects.name AS TableName, ep.name AS PropertyName, "
+        sql += " ep.value AS table_desc"
+        sql += " From sys.objects "
+        sql += " CROSS APPLY fn_listextendedproperty(default,'SCHEMA', schema_name(schema_id),'TABLE', name, null, null) ep"
+        sql += " WHERE sys.objects.name NOT IN ('sysdiagrams') "
+        sql += " and sys.objects.name='" & TableName & "' "
+        sql += " ORDER BY sys.objects.name"
+
+        Dim conn As SqlConnection = SqlDB.GetConnection(GetConnectionString)
+        Dim dt As DataTable = SqlDB.ExecuteTable(sql, conn)
+
+        If dt.Rows.Count > 0 Then
+            If Convert.IsDBNull(dt.Rows(0)("table_desc")) = False Then
+                ret = dt.Rows(0)("table_desc")
+            End If
+        End If
+        Return ret
+
+    End Function
+
+    Private Function GetColumnComment(TableName As String, ColumnName As String) As String
+        Dim ret As String = ""
+        Dim sql As String = "select sep.value column_desc "
+        sql += " From sys.tables st "
+        sql += " inner join sys.columns sc On st.object_id = sc.object_id "
+        sql += " left join sys.extended_properties sep on st.object_id = sep.major_id "
+        sql += "                                 And sc.column_id = sep.minor_id "
+        sql += "                                  And sep.name = 'MS_Description'"
+        sql += " where st.name ='" & TableName & "'"
+        sql += " and sc.name = '" & ColumnName & "'"
+
+        Dim conn As SqlConnection = SqlDB.GetConnection(GetConnectionString)
+        Dim dt As DataTable = SqlDB.ExecuteTable(sql, conn)
+
+        If dt.Rows.Count > 0 Then
+            If Convert.IsDBNull(dt.Rows(0)("column_desc")) = False Then
+                ret = dt.Rows(0)("column_desc")
+            End If
+        End If
+        Return ret
+    End Function
+    Private Function GetFormatColumnTypeName(DataType As String, DataLength As Long) As String
+        Dim ret As String = DataType
+
+        Dim vTypeName As String = DataType.ToUpper
+        If vTypeName = "VARCHAR" Then
+            ret = DataType & "(" & DataLength & ")"
+        ElseIf vTypeName = "CHAR" Then
+            ret = DataType & "(" & DataLength & ")"
+        ElseIf vTypeName = "NVARCHAR" Then
+            ret = DataType & "(" & DataLength & ")"
+        ElseIf vTypeName = "NCHAR" Then
+            ret = DataType & "(" & DataLength & ")"
+        ElseIf vTypeName = "TEXT" Then
+            ret = DataType
+        ElseIf vTypeName = "FLOAT" Then
+            ret = DataType
+        ElseIf vTypeName = "DOUBLE" Then
+            ret = DataType
+        ElseIf vTypeName = "DECIMAL" Then
+            ret = DataType
+        ElseIf vTypeName = "BIGINT" Then
+            ret = DataType
+        ElseIf vTypeName = "BIGINT IDENTITY" Then
+            ret = DataType
+        ElseIf vTypeName = "INT" Then
+            ret = DataType
+        ElseIf vTypeName = "INT IDENTITY" Then
+            ret = DataType
+        ElseIf vTypeName = "SMALLINT" Then
+            ret = DataType
+        ElseIf vTypeName = "DATETIME" Then
+            ret = DataType
+        ElseIf vTypeName = "DATETIME2" Then
+            ret = DataType
+        ElseIf vTypeName = "DATE" Then
+            ret = DataType
+        ElseIf vTypeName = "BIT" Then
+            ret = DataType
+        ElseIf vTypeName = "UNIQUEIDENTIFIER" Then  'uniqueidentifier
+            ret = DataType
+        ElseIf vTypeName = "IMAGE" Then
+            ret = DataType
+        End If
+
+        Return ret
+    End Function
+
+
+    Private Function GetAllTable() As DataTable
+        Dim ret As New DataTable
+        Try
+            Dim Sql As String = "EXEC SP_TABLES null,null,'" & txtDatabaseName.Text & "'"
+            Dim dt As New DataTable
+            dt = SqlDB.ExecuteTable(Sql, SqlDB.GetConnection(GetConnectionString))
+            dt.DefaultView.RowFilter = "table_owner = 'dbo' and table_type='TABLE' and table_name<>'sysdiagrams'"
+
+            ret = dt.DefaultView.ToTable
+        Catch ex As Exception
+            ret = New DataTable
+        End Try
+
+        Return ret
+    End Function
+
+
+#End Region
+End Class
